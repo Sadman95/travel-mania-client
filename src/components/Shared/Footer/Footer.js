e34c1885615974e52d80ed933199bcd48c7fa784
Sadman95 <mail.sakib.1995@gmail.com>
2021-11-01 02:24:38 +0600

    footer

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -2,10 +2,11 @@ import React from 'react';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import PinterestIcon from '@mui/icons-material/Pinterest';
+import './Footer.css'
 
 const Footer = () => {
     return (
-        <div className='text-light bg-dark p-4 d-flex flex-wrap justify-content-between align-items-center'>
+        <div className='footer text-light bg-dark p-4 d-flex flex-wrap justify-content-between align-items-center'>
             <h3>Travel Mania</h3>
             <div>
                 <span>All Rights Reserved || 2021</span>
